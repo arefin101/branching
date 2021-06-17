96334c274a163344a3889f0ec7e288b475f9a9ed
Arefin <arefink910@gmail.com>
2021-06-17 20:22:00 +0600

    nothing

--- demo.php
+++ demo.php
@@ -1,12 +1,8 @@
 <?php
 class goo(){
 
-    return "welcome to 1st page"
-
-}
-
-class having(){
-
-    return "i have a pen"
+    function error(){
+        return "welcome to 1st page"
+    }
 
 }
